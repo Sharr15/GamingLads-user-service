70f7446a0b3052898daf5fb7687d7ddb78023dfc
Sharr15 <fogsliezeta@hotmail.com>
2021-01-05 15:37:30 +0100

    changed url and props

--- src/main/java/gaminglads/userservice/UserServiceApplication.java
+++ src/main/java/gaminglads/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package gaminglads.userservice;
 import gaminglads.userservice.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
